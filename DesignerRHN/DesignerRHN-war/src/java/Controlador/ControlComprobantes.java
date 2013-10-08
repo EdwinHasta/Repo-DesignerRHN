57f78abe8e4422da219f56c5d84e0a668f23317c
Administrator <Administrator@soporte9>
2013-10-08 07:47:03 -0500

    -Felipphe Triviño'- Día 07-10-2013

--- DesignerRHN/DesignerRHN-war/src/java/Controlador/ControlComprobantes.java
+++ DesignerRHN/DesignerRHN-war/src/java/Controlador/ControlComprobantes.java
@@ -5,7 +5,10 @@ import Entidades.ParametrosEstructuras;
 import Entidades.SolucionesNodos;
 import InterfaceAdministrar.AdministrarComprobantesInterface;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -33,11 +36,20 @@ public class ControlComprobantes implements Serializable {
     private int registroActual;
     //OTROS
     private boolean aceptar, mostrarTodos;
+    private Locale locale = new Locale("es", "CO");
+    private NumberFormat nf = NumberFormat.getCurrencyInstance(locale);
+    //SUBTOTALES
+    private BigDecimal subtotalPago, subtotalDescuento, subtotalPasivo, subtotalGasto, neto;
+    private String Pago, Descuento, Pasivo, Gasto, netoTotal;
 
     public ControlComprobantes() {
         registroActual = 0;
         aceptar = true;
         mostrarTodos = true;
+        subtotalPago = new BigDecimal(0);
+        subtotalDescuento = new BigDecimal(0);
+        subtotalPasivo = new BigDecimal(0);
+        subtotalGasto = new BigDecimal(0);
     }
 
     public void anteriorEmpleado() {
@@ -135,7 +147,7 @@ public class ControlComprobantes implements Serializable {
             if (listaParametros != null) {
                 parametroActual = listaParametros.get(registroActual);
             }
-        } 
+        }
         return parametroActual;
     }
 
@@ -192,6 +204,21 @@ public class ControlComprobantes implements Serializable {
     public List<SolucionesNodos> getListaSolucionesNodosEmpleado() {
         if (listaSolucionesNodosEmpleado == null) {
             listaSolucionesNodosEmpleado = administrarComprobantes.solucionesNodosEmpleado(parametroActual.getEmpleado().getSecuencia());
+            if (listaSolucionesNodosEmpleado != null) {
+                subtotalPago = new BigDecimal(0);
+                subtotalDescuento = new BigDecimal(0);
+                for (int i = 0; i < listaSolucionesNodosEmpleado.size(); i++) {
+                    if (listaSolucionesNodosEmpleado.get(i).getTipo().equals("PAGO")) {
+                        subtotalPago = subtotalPago.add(listaSolucionesNodosEmpleado.get(i).getValor());
+                    } else {
+                        subtotalDescuento = subtotalDescuento.add(listaSolucionesNodosEmpleado.get(i).getValor());
+                    }
+                }
+                neto = subtotalPago.subtract(subtotalDescuento);
+                Pago = nf.format(subtotalPago);
+                Descuento = nf.format(subtotalDescuento);
+                netoTotal = nf.format(neto);
+            }
         }
         return listaSolucionesNodosEmpleado;
     }
@@ -211,6 +238,19 @@ public class ControlComprobantes implements Serializable {
     public List<SolucionesNodos> getListaSolucionesNodosEmpleador() {
         if (listaSolucionesNodosEmpleador == null) {
             listaSolucionesNodosEmpleador = administrarComprobantes.solucionesNodosEmpleador(parametroActual.getEmpleado().getSecuencia());
+            if (listaSolucionesNodosEmpleador != null) {
+                subtotalPasivo = new BigDecimal(0);
+                subtotalGasto = new BigDecimal(0);
+                for (int i = 0; i < listaSolucionesNodosEmpleador.size(); i++) {
+                    if (listaSolucionesNodosEmpleador.get(i).getTipo().equals("PASIVO")) {
+                        subtotalPasivo = subtotalPasivo.add(listaSolucionesNodosEmpleador.get(i).getValor());
+                    } else if (listaSolucionesNodosEmpleador.get(i).getTipo().equals("GASTO")) {
+                        subtotalGasto = subtotalGasto.add(listaSolucionesNodosEmpleador.get(i).getValor());
+                    }
+                }
+                Pasivo = nf.format(subtotalPasivo);
+                Gasto = nf.format(subtotalGasto);
+            }
         }
         return listaSolucionesNodosEmpleador;
     }
@@ -225,5 +265,25 @@ public class ControlComprobantes implements Serializable {
 
     public void setFiltradolistaSolucionesNodosEmpleador(List<SolucionesNodos> filtradolistaSolucionesNodosEmpleador) {
         this.filtradolistaSolucionesNodosEmpleador = filtradolistaSolucionesNodosEmpleador;
+    }
+
+    public String getPago() {
+        return Pago;
+    }
+
+    public String getDescuento() {
+        return Descuento;
+    }
+
+    public String getPasivo() {
+        return Pasivo;
+    }
+
+    public String getGasto() {
+        return Gasto;
+    }
+
+    public String getNetoTotal() {
+        return netoTotal;
     }
 }
