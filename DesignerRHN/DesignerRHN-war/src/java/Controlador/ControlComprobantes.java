20f306d8b6ab7ea92ab709dbd31fbede39842e50
Administrator <Administrator@soporte9>
2013-10-09 07:51:05 -0500

    -Felipphe Triviño'- Día 07-10-2013

--- DesignerRHN/DesignerRHN-war/src/java/Controlador/ControlComprobantes.java
+++ DesignerRHN/DesignerRHN-war/src/java/Controlador/ControlComprobantes.java
@@ -3,7 +3,10 @@ package Controlador;
 import Entidades.Parametros;
 import Entidades.ParametrosEstructuras;
 import Entidades.SolucionesNodos;
+import Exportar.ExportarPDFTablasAnchas;
+import Exportar.ExportarXLS;
 import InterfaceAdministrar.AdministrarComprobantesInterface;
+import java.io.IOException;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.text.NumberFormat;
@@ -12,6 +15,10 @@ import java.util.Locale;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.component.column.Column;
+import org.primefaces.component.datatable.DataTable;
+import org.primefaces.component.export.Exporter;
 import org.primefaces.context.RequestContext;
 
 @ManagedBean
@@ -41,6 +48,13 @@ public class ControlComprobantes implements Serializable {
     //SUBTOTALES
     private BigDecimal subtotalPago, subtotalDescuento, subtotalPasivo, subtotalGasto, neto;
     private String Pago, Descuento, Pasivo, Gasto, netoTotal;
+    //FILTRADO
+    private Column codigoSNE, descipcionSNE, unidadSNE, pagoSNE, descuentoSNE, terceroSNE, fechaHastaSNE, debitoSNE, centroCostoDSNE, creditoSNE, centroCostoCSNE, saldoSNE, fechaDesdeSNE, fechaPagoSNE, FechaModificacioSNE;
+    private Column codigoSNER, descipcionSNER, unidadSNER, pasivoSNER, gastoSNER, terceroSNER, fechaHastaSNER, debitoSNER, centroCostoDSNER, creditoSNER, centroCostoCSNER, saldoSNER, fechaDesdeSNER, fechaPagoSNER, FechaModificacioSNER;
+    private int bandera;
+    private String altoScrollSolucionesNodosEmpleado, altoScrollSolucionesNodosEmpleador;
+    //PARCIALES 
+    private String parcialesSolucionNodos;
 
     public ControlComprobantes() {
         registroActual = 0;
@@ -50,6 +64,9 @@ public class ControlComprobantes implements Serializable {
         subtotalDescuento = new BigDecimal(0);
         subtotalPasivo = new BigDecimal(0);
         subtotalGasto = new BigDecimal(0);
+        bandera = 0;
+        altoScrollSolucionesNodosEmpleado = "105";
+        altoScrollSolucionesNodosEmpleador = "105";
     }
 
     public void anteriorEmpleado() {
@@ -125,6 +142,190 @@ public class ControlComprobantes implements Serializable {
         context.update("form:panelInf");
         context.update("form:datosSolucionesNodosEmpleado");
         context.update("form:datosSolucionesNodosEmpleador");
+    }
+
+    //CTRL + F11 ACTIVAR/DESACTIVAR
+    public void activarCtrlF11() {
+        if (bandera == 0) {
+            //SOLUCIONES NODOS EMPLEADO
+            codigoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:codigoSNE");
+            codigoSNE.setFilterStyle("width: 40px");
+            descipcionSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:descipcionSNE");
+            descipcionSNE.setFilterStyle("width: 170px");
+            unidadSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:unidadSNE");
+            unidadSNE.setFilterStyle("width: 30px");
+            pagoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:pagoSNE");
+            pagoSNE.setFilterStyle("");
+            descuentoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:descuentoSNE");
+            descuentoSNE.setFilterStyle("");
+            terceroSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:terceroSNE");
+            terceroSNE.setFilterStyle("width: 180px");
+            fechaHastaSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:fechaHastaSNE");
+            fechaHastaSNE.setFilterStyle("width: 40px");
+            debitoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:debitoSNE");
+            debitoSNE.setFilterStyle("width: 50px");
+            centroCostoDSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:centroCostoDSNE");
+            centroCostoDSNE.setFilterStyle("width: 80px");
+            creditoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:creditoSNE");
+            creditoSNE.setFilterStyle("width: 50px");
+            centroCostoCSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:centroCostoCSNE");
+            centroCostoCSNE.setFilterStyle("width: 80px");
+            saldoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:saldoSNE");
+            saldoSNE.setFilterStyle("width: 80px");
+            fechaDesdeSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:fechaDesdeSNE");
+            fechaDesdeSNE.setFilterStyle("width: 40px");
+            fechaPagoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:fechaPagoSNE");
+            fechaPagoSNE.setFilterStyle("width: 40px");
+            FechaModificacioSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:FechaModificacioSNE");
+            FechaModificacioSNE.setFilterStyle("width: 70px");
+
+            //SOLUCIONES NODOS EMPLEADOR
+            codigoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:codigoSNER");
+            codigoSNER.setFilterStyle("width: 40px");
+            descipcionSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:descipcionSNER");
+            descipcionSNER.setFilterStyle("width: 170px");
+            unidadSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:unidadSNER");
+            unidadSNER.setFilterStyle("width: 30px");
+            pasivoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:pasivoSNER");
+            pasivoSNER.setFilterStyle("");
+            gastoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:gastoSNER");
+            gastoSNER.setFilterStyle("");
+            terceroSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:terceroSNER");
+            terceroSNER.setFilterStyle("width: 180px");
+            fechaHastaSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:fechaHastaSNER");
+            fechaHastaSNER.setFilterStyle("width: 40px");
+            debitoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:debitoSNER");
+            debitoSNER.setFilterStyle("width: 50px");
+            centroCostoDSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:centroCostoDSNER");
+            centroCostoDSNER.setFilterStyle("width: 80px");
+            creditoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:creditoSNER");
+            creditoSNER.setFilterStyle("width: 50px");
+            centroCostoCSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:centroCostoCSNER");
+            centroCostoCSNER.setFilterStyle("width: 80px");
+            saldoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:saldoSNER");
+            saldoSNER.setFilterStyle("width: 80px");
+            fechaDesdeSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:fechaDesdeSNER");
+            fechaDesdeSNER.setFilterStyle("width: 40px");
+            fechaPagoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:fechaPagoSNER");
+            fechaPagoSNER.setFilterStyle("width: 40px");
+            FechaModificacioSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:FechaModificacioSNER");
+            FechaModificacioSNER.setFilterStyle("width: 70px");
+
+            altoScrollSolucionesNodosEmpleado = "81";
+            altoScrollSolucionesNodosEmpleador = "81";
+            RequestContext context = RequestContext.getCurrentInstance();
+            context.update("form:datosSolucionesNodosEmpleado");
+            context.update("form:datosSolucionesNodosEmpleador");
+            bandera = 1;
+
+        } else if (bandera == 1) {
+            //SOLUCIONES NODOS EMPLEADO
+            codigoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:codigoSNE");
+            codigoSNE.setFilterStyle("display: none; visibility: hidden;");
+            descipcionSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:descipcionSNE");
+            descipcionSNE.setFilterStyle("display: none; visibility: hidden;");
+            unidadSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:unidadSNE");
+            unidadSNE.setFilterStyle("display: none; visibility: hidden;");
+            pagoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:pagoSNE");
+            pagoSNE.setFilterStyle("display: none; visibility: hidden;");
+            descuentoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:descuentoSNE");
+            descuentoSNE.setFilterStyle("display: none; visibility: hidden;");
+            terceroSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:terceroSNE");
+            terceroSNE.setFilterStyle("display: none; visibility: hidden;");
+            fechaHastaSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:fechaHastaSNE");
+            fechaHastaSNE.setFilterStyle("display: none; visibility: hidden;");
+            debitoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:debitoSNE");
+            debitoSNE.setFilterStyle("display: none; visibility: hidden;");
+            centroCostoDSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:centroCostoDSNE");
+            centroCostoDSNE.setFilterStyle("display: none; visibility: hidden;");
+            creditoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:creditoSNE");
+            creditoSNE.setFilterStyle("display: none; visibility: hidden;");
+            centroCostoCSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:centroCostoCSNE");
+            centroCostoCSNE.setFilterStyle("display: none; visibility: hidden;");
+            saldoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:saldoSNE");
+            saldoSNE.setFilterStyle("display: none; visibility: hidden;");
+            fechaDesdeSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:fechaDesdeSNE");
+            fechaDesdeSNE.setFilterStyle("display: none; visibility: hidden;");
+            fechaPagoSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:fechaPagoSNE");
+            fechaPagoSNE.setFilterStyle("display: none; visibility: hidden;");
+            FechaModificacioSNE = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleado:FechaModificacioSNE");
+            FechaModificacioSNE.setFilterStyle("display: none; visibility: hidden;");
+
+            //SOLUCIONES NODOS EMPLEADOR
+            codigoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:codigoSNER");
+            codigoSNER.setFilterStyle("display: none; visibility: hidden;");
+            descipcionSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:descipcionSNER");
+            descipcionSNER.setFilterStyle("display: none; visibility: hidden;");
+            unidadSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:unidadSNER");
+            unidadSNER.setFilterStyle("display: none; visibility: hidden;");
+            pasivoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:pasivoSNER");
+            pasivoSNER.setFilterStyle("display: none; visibility: hidden;");
+            gastoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:gastoSNER");
+            gastoSNER.setFilterStyle("display: none; visibility: hidden;");
+            terceroSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:terceroSNER");
+            terceroSNER.setFilterStyle("display: none; visibility: hidden;");
+            fechaHastaSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:fechaHastaSNER");
+            fechaHastaSNER.setFilterStyle("display: none; visibility: hidden;");
+            debitoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:debitoSNER");
+            debitoSNER.setFilterStyle("display: none; visibility: hidden;");
+            centroCostoDSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:centroCostoDSNER");
+            centroCostoDSNER.setFilterStyle("display: none; visibility: hidden;");
+            creditoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:creditoSNER");
+            creditoSNER.setFilterStyle("display: none; visibility: hidden;");
+            centroCostoCSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:centroCostoCSNER");
+            centroCostoCSNER.setFilterStyle("display: none; visibility: hidden;");
+            saldoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:saldoSNER");
+            saldoSNER.setFilterStyle("display: none; visibility: hidden;");
+            fechaDesdeSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:fechaDesdeSNER");
+            fechaDesdeSNER.setFilterStyle("display: none; visibility: hidden;");
+            fechaPagoSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:fechaPagoSNER");
+            fechaPagoSNER.setFilterStyle("display: none; visibility: hidden;");
+            FechaModificacioSNER = (Column) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:datosSolucionesNodosEmpleador:FechaModificacioSNER");
+            FechaModificacioSNER.setFilterStyle("display: none; visibility: hidden;");
+
+            altoScrollSolucionesNodosEmpleado = "105";
+            altoScrollSolucionesNodosEmpleador = "105";
+            RequestContext context = RequestContext.getCurrentInstance();
+            context.update("form:datosSolucionesNodosEmpleado");
+            context.update("form:datosSolucionesNodosEmpleador");
+            bandera = 0;
+            filtradolistaSolucionesNodosEmpleado = null;
+        }
+    }
+
+    public void exportPDF() throws IOException {
+        DataTable tabla;
+        Exporter exporter = new ExportarPDFTablasAnchas();
+        FacesContext context = FacesContext.getCurrentInstance();
+        System.out.println("1");
+        tabla = (DataTable) FacesContext.getCurrentInstance().getViewRoot().findComponent("formExportar:datosSolucionesNodosEmpleadoExportar");
+        exporter.export(context, tabla, "SolucionesNodosEmpleadoPDF", false, false, "UTF-8", null, null);
+        context.responseComplete();
+        System.out.println("2");
+        tabla = (DataTable) FacesContext.getCurrentInstance().getViewRoot().findComponent("formExportar:datosSolucionesNodosEmpleadorExportar");
+        exporter.export(context, tabla, "SolucionesNodosEmpleadorPDF", false, false, "UTF-8", null, null);
+        context.responseComplete();
+    }
+
+    public void exportXLS() throws IOException {
+        DataTable tabla;
+        Exporter exporter = new ExportarXLS();
+        FacesContext context = FacesContext.getCurrentInstance();
+        tabla = (DataTable) FacesContext.getCurrentInstance().getViewRoot().findComponent("formExportar:datosSolucionesNodosEmpleadoExportar");
+        exporter.export(context, tabla, "SolucionesNodosEmpleadoXLS", false, false, "UTF-8", null, null);
+        context.responseComplete();
+    }
+
+    //PARCIALES CONCEPTO
+    public void parcialSolucionNodo(int indice, int tabla) {
+        if (tabla == 0) {
+            parcialesSolucionNodos = listaSolucionesNodosEmpleado.get(indice).getParciales();
+        } else if (tabla == 1) {
+            parcialesSolucionNodos = listaSolucionesNodosEmpleador.get(indice).getParciales();
+        }
+        RequestContext context = RequestContext.getCurrentInstance();
+        context.update("formularioDialogos:parcialesConcepto");
+        context.execute("parcialesConcepto.show();");
     }
 
     public void activarAceptar() {
@@ -285,5 +486,17 @@ public class ControlComprobantes implements Serializable {
 
     public String getNetoTotal() {
         return netoTotal;
+    }
+
+    public String getAltoScrollSolucionesNodosEmpleado() {
+        return altoScrollSolucionesNodosEmpleado;
+    }
+
+    public String getAltoScrollSolucionesNodosEmpleador() {
+        return altoScrollSolucionesNodosEmpleador;
+    }
+
+    public String getParcialesSolucionNodos() {
+        return parcialesSolucionNodos;
     }
 }
