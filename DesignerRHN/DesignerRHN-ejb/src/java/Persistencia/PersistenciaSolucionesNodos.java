c61a6dc07bb4eeafcce45a9e59f442bc75970001
HugoSin89DSW <hsin@nomina.com.co>
2013-12-13 14:17:40 -0500

    Hugo David Sin Gutiérres - Solución errores por documetación- 13/12/2013

--- DesignerRHN/DesignerRHN-ejb/src/java/Persistencia/PersistenciaSolucionesNodos.java
+++ DesignerRHN/DesignerRHN-ejb/src/java/Persistencia/PersistenciaSolucionesNodos.java
@@ -1,3 +1,6 @@
+/**
+ * Documentación a cargo de Hugo David Sin Gutiérrez
+ */
 package Persistencia;
 
 import Entidades.SolucionesNodos;
@@ -10,48 +13,35 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-
+/**
+ * Clase Stateless 
+ * Clase encargada de realizar operaciones sobre la tabla 'SolucionesNodos'
+ * de la base de datos.
+ * @author betelgeuse
+ */
 @Stateless
 public class PersistenciaSolucionesNodos implements PersistenciaSolucionesNodosInterface {
-
+    /**
+     * Atributo EntityManager. Representa la comunicación con la base de datos.
+     */
     @PersistenceContext(unitName = "DesignerRHN-ejbPU")
     private EntityManager em;
 
-    /*
-     * Crear Solucion Nodo.
-     */
     @Override
     public void crear(SolucionesNodos solucionNodo) {
         em.persist(solucionNodo);
     }
 
-    /*
-     *Editar Solucion Nodo. 
-     */
     @Override
     public void editar(SolucionesNodos solucionNodo) {
         em.merge(solucionNodo);
     }
 
-    /*
-     *Borrar Solucion Nodo.
-     */
     @Override
     public void borrar(SolucionesNodos solucionNodo) {
         em.remove(em.merge(solucionNodo));
     }
 
-    /*
-     *Encontrar una Solucion Nodo. 
-     */
-    @Override
-    public SolucionesNodos buscarSolucionNodo(Object id) {
-        return em.find(SolucionesNodos.class, id);
-    }
-
-    /*
-     *Encontrar todas las Soluciones Nodos. 
-     */
     @Override
     public List<SolucionesNodos> buscarSolucionesNodos() {
         javax.persistence.criteria.CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
@@ -90,7 +80,23 @@ public class PersistenciaSolucionesNodos implements PersistenciaSolucionesNodosI
     @Override
     public BigDecimal diasProvisionados(BigInteger secuencia) {
         try {
-            Query query = em.createQuery("SELECT SN1.unidades FROM SolucionesNodos SN1, Conceptos c WHERE SN1.empleado.secuencia =:secuenciaempleado AND c.secuencia=SN1.concepto.secuencia AND SN1.tipo='PASIVO' AND SN1.estado='CERRADO' AND SN1.concepto.secuencia=(SELECT GC.concepto.secuencia FROM  GruposProvisiones GC WHERE GC.nombre='VACACIONES' AND GC.empresa.secuencia=c.empresa.secuencia) AND SN1.fechahasta = (SELECT MAX(SN2.fechahasta) FROM SolucionesNodos SN2 WHERE SN2.fechadesde<=CURRENT_DATE AND SN1.empleado.secuencia=SN2.empleado.secuencia AND SN1.concepto.secuencia=SN2.concepto.secuencia AND SN2.tipo = 'PASIVO' AND SN2.estado='CERRADO')");
+            Query query = em.createQuery("SELECT SN1.unidades "
+                                        + "FROM SolucionesNodos SN1, Conceptos c "
+                                        + "WHERE SN1.empleado.secuencia =:secuenciaempleado "
+                                        + "AND c.secuencia=SN1.concepto.secuencia "
+                                        + "AND SN1.tipo='PASIVO' "
+                                        + "AND SN1.estado='CERRADO' "
+                                        + "AND SN1.concepto.secuencia=(SELECT GC.concepto.secuencia "
+                                                                    + "FROM  GruposProvisiones GC "
+                                                                    + "WHERE GC.nombre='VACACIONES' "
+                                                                    + "AND GC.empresa.secuencia=c.empresa.secuencia) "
+                                        + "AND SN1.fechahasta = (SELECT MAX(SN2.fechahasta) "
+                                                              + "FROM SolucionesNodos SN2 "
+                                                              + "WHERE SN2.fechadesde<=CURRENT_DATE "
+                                                              + "AND SN1.empleado.secuencia=SN2.empleado.secuencia "
+                                                              + "AND SN1.concepto.secuencia=SN2.concepto.secuencia "
+                                                              + "AND SN2.tipo = 'PASIVO' "
+                                                              + "AND SN2.estado='CERRADO')");
             query.setParameter("secuenciaempleado", secuencia);
             BigDecimal llegada = (BigDecimal) query.getSingleResult();
             return llegada;
@@ -103,7 +109,18 @@ public class PersistenciaSolucionesNodos implements PersistenciaSolucionesNodosI
     @Override
     public Long validacionTercerosVigenciaAfiliacion(BigInteger secuencia, Date fechaInicial, BigDecimal secuenciaTE, BigInteger secuenciaTer) {
         try {
-            Query query = em.createQuery("SELECT count(v)  FROM SolucionesNodos v where v.fechapago > :fechaInicial AND v.empleado.secuencia = :secuencia AND v.estado ='CERRADO' AND V.secuencia != :secuenciaTer AND exists (SELECT cs FROM ConceptosSoportes cs WHERE cs.concepto.secuencia = v.concepto.secuencia AND cs.tipoentidad.secuencia = :secuenciaT and cs.tipo='AUTOLIQUIDACION' AND cs.subgrupo='COTIZACION')");
+            Query query = em.createQuery("SELECT count(v)  "
+                                       + "FROM SolucionesNodos v "
+                                       + "where v.fechapago > :fechaInicial "
+                                       + "AND v.empleado.secuencia = :secuencia "
+                                       + "AND v.estado ='CERRADO' "
+                                       + "AND v.tercero.secuencia != :secuenciaTer "
+                                       + "AND exists (SELECT cs "
+                                                   + "FROM ConceptosSoportes cs "
+                                                   + "WHERE cs.concepto.secuencia = v.concepto.secuencia "
+                                                   + "AND cs.tipoentidad.secuencia = :secuenciaT "
+                                                   + "and cs.tipo='AUTOLIQUIDACION' "
+                                                   + "AND cs.subgrupo='COTIZACION')");
             query.setParameter("secuencia", secuencia);
             query.setParameter("fechaInicial", fechaInicial);
             query.setParameter("secuenciaT", secuenciaTE);
@@ -120,7 +137,12 @@ public class PersistenciaSolucionesNodos implements PersistenciaSolucionesNodosI
     @Override
     public List<SolucionesNodos> solucionNodoEmpleado(BigInteger secuenciaEmpleado) {
         try {
-            Query query = em.createQuery("SELECT sn FROM SolucionesNodos sn WHERE sn.estado = 'LIQUIDADO' AND sn.tipo IN ('PAGO','DESCUENTO') AND sn.empleado.secuencia = :secuenciaEmpleado ORDER BY sn.concepto.codigo ASC");
+            Query query = em.createQuery("SELECT sn "
+                                       + "FROM SolucionesNodos sn "
+                                       + "WHERE sn.estado = 'LIQUIDADO' "
+                                       + "AND sn.tipo IN ('PAGO','DESCUENTO') "
+                                       + "AND sn.empleado.secuencia = :secuenciaEmpleado "
+                                       + "ORDER BY sn.concepto.codigo ASC");
             query.setParameter("secuenciaEmpleado", secuenciaEmpleado);
             List<SolucionesNodos> listSolucionesNodos = query.getResultList();
             return listSolucionesNodos;
@@ -133,7 +155,12 @@ public class PersistenciaSolucionesNodos implements PersistenciaSolucionesNodosI
     @Override
     public List<SolucionesNodos> solucionNodoEmpleador(BigInteger secuenciaEmpleado) {
         try {
-            Query query = em.createQuery("SELECT sn FROM SolucionesNodos sn WHERE sn.estado = 'LIQUIDADO' AND sn.tipo IN  ('PASIVO','GASTO','NETO') AND sn.valor <> 0 AND sn.empleado.secuencia = :secuenciaEmpleado ORDER BY sn.concepto.codigo ASC");
+            Query query = em.createQuery("SELECT sn "
+                                       + "FROM SolucionesNodos sn "
+                                       + "WHERE sn.estado = 'LIQUIDADO' "
+                                       + "AND sn.tipo IN  ('PASIVO','GASTO','NETO') "
+                                       + "AND sn.valor <> 0 AND sn.empleado.secuencia = :secuenciaEmpleado "
+                                       + "ORDER BY sn.concepto.codigo ASC");
             query.setParameter("secuenciaEmpleado", secuenciaEmpleado);
             List<SolucionesNodos> listSolucionesNodos = query.getResultList();
             return listSolucionesNodos;
@@ -143,6 +170,7 @@ public class PersistenciaSolucionesNodos implements PersistenciaSolucionesNodosI
         }
     }
 
+    @Override
     public Integer ContarProcesosSN(BigInteger secProceso) {
         try {
             String sqlQuery = "SELECT COUNT(distinct empleado)\n"
