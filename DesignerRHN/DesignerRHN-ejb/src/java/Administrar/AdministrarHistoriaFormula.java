01c9c6021e6f48e58a3f5c28e95186f75194f69a
PROYECTO01 <PROYECTO01@PROYECTO01>
2013-12-30 17:44:33 -0500

    AndresPineda 30-12-2013 >> EJB

--- DesignerRHN/DesignerRHN-ejb/src/java/Administrar/AdministrarHistoriaFormula.java
+++ DesignerRHN/DesignerRHN-ejb/src/java/Administrar/AdministrarHistoriaFormula.java
@@ -53,6 +53,8 @@ public class AdministrarHistoriaFormula implements AdministrarHistoriaFormulaInt
     public void crearHistoriasFormulas(List<Historiasformulas> lista) {
         try {
             for (int i = 0; i < lista.size(); i++) {
+                String aux = lista.get(i).getObservaciones().toUpperCase();
+                lista.get(i).setObservaciones(aux);
                 persistenciaHistoriasFormulas.crear(lista.get(i));
             }
         } catch (Exception e) {
@@ -64,6 +66,8 @@ public class AdministrarHistoriaFormula implements AdministrarHistoriaFormulaInt
     public void editarHistoriasFormulas(List<Historiasformulas> lista) {
         try {
             for (int i = 0; i < lista.size(); i++) {
+                String aux = lista.get(i).getObservaciones().toUpperCase();
+                lista.get(i).setObservaciones(aux);
                 persistenciaHistoriasFormulas.editar(lista.get(i));
             }
         } catch (Exception e) {
@@ -74,7 +78,7 @@ public class AdministrarHistoriaFormula implements AdministrarHistoriaFormulaInt
     @Override
     public void borrarHistoriasFormulas(List<Historiasformulas> lista) {
         try {
-            
+
             for (int i = 0; i < lista.size(); i++) {
                 persistenciaHistoriasFormulas.borrar(lista.get(i));
             }
@@ -96,8 +100,8 @@ public class AdministrarHistoriaFormula implements AdministrarHistoriaFormulaInt
 
     public void crearNodos(List<Nodos> lista) {
         try {
-            for(int i = 0;i<lista.size();i++){
-                System.out.println("Nivel : "+lista.get(i).getPosicion());
+            for (int i = 0; i < lista.size(); i++) {
+                System.out.println("Nivel : " + lista.get(i).getPosicion());
             }
             for (int i = 0; i < lista.size(); i++) {
                 if (lista.get(i).getOperador().getSecuencia() == null) {
@@ -106,7 +110,7 @@ public class AdministrarHistoriaFormula implements AdministrarHistoriaFormulaInt
                 if (lista.get(i).getOperando().getSecuencia() == null) {
                     lista.get(i).setOperando(null);
                 }
-                
+                System.out.println("lista.get(i) Crear : " + lista.get(i).getSecuencia());
                 persistenciaNodos.crear(lista.get(i));
             }
         } catch (Exception e) {
@@ -116,8 +120,8 @@ public class AdministrarHistoriaFormula implements AdministrarHistoriaFormulaInt
 
     public void borrarNodos(List<Nodos> lista) {
         try {
-            for(int i = 0;i<lista.size();i++){
-                System.out.println("Nivel : "+lista.get(i).getPosicion());
+            for (int i = 0; i < lista.size(); i++) {
+                System.out.println("Nivel : " + lista.get(i).getPosicion());
             }
             for (int i = 0; i < lista.size(); i++) {
                 if (lista.get(i).getOperador().getSecuencia() == null) {
