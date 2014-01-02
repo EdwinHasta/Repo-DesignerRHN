1f8ef14614df1ac7cc1c53383a60d57e81c244e0
user <user@user-PC>
2014-01-02 13:25:54 -0500

    Merge origin/master
    Conflicts:
    	DesignerRHN/DesignerRHN-ejb/nbproject/project.properties
    	DesignerRHN/DesignerRHN-ejb/src/java/Administrar/AdministrarCentroCostos.java
    	DesignerRHN/DesignerRHN-ejb/src/java/Entidades/SoActosInseguros.java
    	DesignerRHN/DesignerRHN-ejb/src/java/InterfaceAdministrar/AdministrarCentroCostosInterface.java
    	DesignerRHN/DesignerRHN-war/nbproject/faces-config.NavData
    	DesignerRHN/DesignerRHN-war/nbproject/project.properties
    	DesignerRHN/DesignerRHN-war/web/centrocosto.xhtml
    	DesignerRHN/nbproject/project.properties

---
+++ DesignerRHN/DesignerRHN-ejb/src/java/Persistencia/PersistenciaEvalDimensiones.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistencia;
+
+import InterfacePersistencia.PersistenciaEvalDimensionesInterface;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import Entidades.EvalDimensiones;
+import java.math.BigInteger;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author user
+ */
+@Stateless
+public class PersistenciaEvalDimensiones implements PersistenciaEvalDimensionesInterface {
+
+    /**
+     * Atributo EntityManager. Representa la comunicación con la base de datos
+     */
+    @PersistenceContext(unitName = "DesignerRHN-ejbPU")
+    private EntityManager em;
+
+    public void crear(EvalDimensiones evalDimensiones) {
+        em.persist(evalDimensiones);
+    }
+
+    public void editar(EvalDimensiones evalDimensiones) {
+        em.merge(evalDimensiones);
+    }
+
+    public void borrar(EvalDimensiones evalDimensiones) {
+        em.remove(em.merge(evalDimensiones));
+    }
+
+    public EvalDimensiones buscarEvalDimension(BigInteger secuencia) {
+        try {
+            return em.find(EvalDimensiones.class, secuencia);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public List<EvalDimensiones> buscarEvalDimensiones() {
+        try {
+            Query query = em.createQuery("SELECT ed FROM EvalDimensiones ed ORDER BY ed.descripcion ");
+            List<EvalDimensiones> evalDimensiones = query.getResultList();
+            return evalDimensiones;
+        } catch (Exception e) {
+            System.err.println("PERSISTENCIAEVALDIMENSIONES BUSCAREVALDIMENSIONES ERROR : " + e);
+            return null;
+        }
+    }
+    
+    public BigInteger contradorEvalPlanillas(BigInteger secuencia) {
+        BigInteger retorno;
+        try {
+            String sqlQuery = "SELECT COUNT(*)FROM evaldimensiones ev, evalplanillas ep  WHERE ep.dimension=ev.secuencia AND ev.secuencia = ? ";
+            Query query = em.createNativeQuery(sqlQuery);
+            query.setParameter(1, secuencia);
+            retorno = new BigInteger(query.getSingleResult().toString());
+            System.out.println("PERSISTENCIAEVALDIMENSIONES contradorEvalPlanillas = " + retorno);
+            return retorno;
+        } catch (Exception e) {
+            System.err.println("ERROR PERSISTENCIAEVALDIMENSIONES contradorEvalPlanillas  ERROR = " + e);
+            retorno = new BigInteger("-1");
+            return retorno;
+        }
+    }
+}
