0d343e2f83698df32ed8e52a41d3c5a860ae176f
Administrador <Administrador@SOPORTE9>
2014-01-03 08:19:28 -0500

    Victor Algarin 2/2/2014

--- DesignerRHN/DesignerRHN-war/src/java/Validaciones/Validar.java
+++ DesignerRHN/DesignerRHN-war/src/java/Validaciones/Validar.java
@@ -5,7 +5,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.validator.FacesValidator;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
-import javax.validation.executable.ExecutableValidator;
 import javax.validation.metadata.BeanDescriptor;
 
 @ManagedBean
@@ -40,8 +39,5 @@ public class Validar implements Validator {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public ExecutableValidator forExecutables() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+
 }
