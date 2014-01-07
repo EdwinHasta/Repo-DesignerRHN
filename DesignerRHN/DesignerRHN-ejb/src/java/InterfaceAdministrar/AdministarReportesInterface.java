9d4e3232d993e0c8d9b22c33f184c41a36e43123
HugoSin89DSW <hsin@nomina.com.co>
2014-01-07 09:42:15 -0500

    Hugo David Sin Gutiérres - Documentación Administrar - 07/01/2014

--- DesignerRHN/DesignerRHN-ejb/src/java/InterfaceAdministrar/AdministarReportesInterface.java
+++ DesignerRHN/DesignerRHN-ejb/src/java/InterfaceAdministrar/AdministarReportesInterface.java
@@ -1,22 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Documentación a cargo de Hugo David Sin Gutiérrez
  */
-
 package InterfaceAdministrar;
 
-import Entidades.Empleados;
 import java.math.BigInteger;
-import java.util.List;
 
 /**
- *
- * @author Administrator
+ * Interface encargada de determinar las operaciones que se realizan para realizar reportes. 
+ * de la base de datos.
+ * @author betelgeuse
  */
 public interface AdministarReportesInterface {
+    /**
+     * Método encargado de recuperar los datos de conexión del usuario ya seteado.<br>
+     * El usuario esta seteado cuando esta asociado a un perfil del aplicativo.
+     */
     public void datosConexion();
+    /**
+     * Método encargado de generar un comprobante (PDF) para un Empleado específico.
+     * @param codigoEmpleado Código del empleado.
+     */
     public void generarReporte(BigInteger codigoEmpleado);
+    /**
+     * Método encargado de generar un reporte XLSX para un Empleado específico.
+     */
     public void generarReporteXLSX();
+    /**
+     * Método encargado de generar un reporte XML para un Empleado específico.
+     */
     public void generarReporteXML();
 }
