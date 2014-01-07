994a0cc7b614fa9607b134fcce07dbd3d50fb342
jspinedam <jspinedam@gmail.com>
2014-01-07 08:44:06 -0500

    Update Elementoscausasaccidentes.java

--- DesignerRHN/DesignerRHN-ejb/src/java/Entidades/Elementoscausasaccidentes.java
+++ DesignerRHN/DesignerRHN-ejb/src/java/Entidades/Elementoscausasaccidentes.java
@@ -5,7 +5,7 @@
 package Entidades;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Collection;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
@@ -17,7 +17,6 @@ import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
@@ -29,53 +28,48 @@ import javax.xml.bind.annotation.XmlTransient;
 @Table(name = "ELEMENTOSCAUSASACCIDENTES")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "Elementoscausasaccidentes.findAll", query = "SELECT e FROM Elementoscausasaccidentes e")})
-public class Elementoscausasaccidentes implements Serializable {
+    @NamedQuery(name = "ElementosCausasAccidentes.findAll", query = "SELECT e FROM ElementosCausasAccidentes e")})
+public class ElementosCausasAccidentes implements Serializable {
     private static final long serialVersionUID = 1L;
     // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
     @Id
     @Basic(optional = false)
     @NotNull
     @Column(name = "SECUENCIA")
-    private BigDecimal secuencia;
-    @Basic(optional = false)
-    @NotNull
+    private BigInteger secuencia;
     @Column(name = "CODIGO")
-    private short codigo;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 1000)
+    private Integer codigo;
     @Column(name = "DESCRIPCION")
     private String descripcion;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "causa")
     private Collection<Soaccidentes> soaccidentesCollection;
 
-    public Elementoscausasaccidentes() {
+    public ElementosCausasAccidentes() {
     }
 
-    public Elementoscausasaccidentes(BigDecimal secuencia) {
+    public ElementosCausasAccidentes(BigInteger secuencia) {
         this.secuencia = secuencia;
     }
 
-    public Elementoscausasaccidentes(BigDecimal secuencia, short codigo, String descripcion) {
+    public ElementosCausasAccidentes(BigInteger secuencia, Integer codigo, String descripcion) {
         this.secuencia = secuencia;
         this.codigo = codigo;
         this.descripcion = descripcion;
     }
 
-    public BigDecimal getSecuencia() {
+    public BigInteger getSecuencia() {
         return secuencia;
     }
 
-    public void setSecuencia(BigDecimal secuencia) {
+    public void setSecuencia(BigInteger secuencia) {
         this.secuencia = secuencia;
     }
 
-    public short getCodigo() {
+    public Integer getCodigo() {
         return codigo;
     }
 
-    public void setCodigo(short codigo) {
+    public void setCodigo(Integer codigo) {
         this.codigo = codigo;
     }
 
@@ -84,7 +78,7 @@ public class Elementoscausasaccidentes implements Serializable {
     }
 
     public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+        this.descripcion = descripcion.toUpperCase();
     }
 
     @XmlTransient
@@ -106,10 +100,10 @@ public class Elementoscausasaccidentes implements Serializable {
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Elementoscausasaccidentes)) {
+        if (!(object instanceof ElementosCausasAccidentes)) {
             return false;
         }
-        Elementoscausasaccidentes other = (Elementoscausasaccidentes) object;
+        ElementosCausasAccidentes other = (ElementosCausasAccidentes) object;
         if ((this.secuencia == null && other.secuencia != null) || (this.secuencia != null && !this.secuencia.equals(other.secuencia))) {
             return false;
         }
