9f06640ae004f4deae7d1c189901542dff643f4e
Administrador <Administrador@SOPORTE9>
2014-01-08 15:57:05 -0500

    Felipphe Triviño' 08-01-2014

---
+++ DesignerRHN/DesignerRHN-ejb/src/java/Entidades/EvalCompetencias.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entidades;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Administrador
+ */
+@Entity
+@Table(name = "EVALCOMPETENCIAS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "EvalCompetencias.findAll", query = "SELECT e FROM EvalCompetencias e")})
+public class EvalCompetencias implements Serializable {
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "SECUENCIA")
+    private BigDecimal secuencia;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CODIGO")
+    private BigInteger codigo;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "DESCRIPCION")
+    private String descripcion;
+    @Size(max = 1000)
+    @Column(name = "DESCOMPETENCIA")
+    private String descompetencia;
+
+    public EvalCompetencias() {
+    }
+
+    public EvalCompetencias(BigDecimal secuencia) {
+        this.secuencia = secuencia;
+    }
+
+    public EvalCompetencias(BigDecimal secuencia, BigInteger codigo, String descripcion) {
+        this.secuencia = secuencia;
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getSecuencia() {
+        return secuencia;
+    }
+
+    public void setSecuencia(BigDecimal secuencia) {
+        this.secuencia = secuencia;
+    }
+
+    public BigInteger getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(BigInteger codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescompetencia() {
+        return descompetencia;
+    }
+
+    public void setDescompetencia(String descompetencia) {
+        this.descompetencia = descompetencia;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (secuencia != null ? secuencia.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof EvalCompetencias)) {
+            return false;
+        }
+        EvalCompetencias other = (EvalCompetencias) object;
+        if ((this.secuencia == null && other.secuencia != null) || (this.secuencia != null && !this.secuencia.equals(other.secuencia))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entidades.EvalCompetencias[ secuencia=" + secuencia + " ]";
+    }
+    
+}
