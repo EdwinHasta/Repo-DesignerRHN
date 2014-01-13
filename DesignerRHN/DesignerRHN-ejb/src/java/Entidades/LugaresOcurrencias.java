7b574a38c505b0c83006ce7b6c9224f9034dfc22
Administrador <Administrador@SOPORTE9>
2014-01-13 17:28:52 -0500

    Felipphe Triviño' 13-01-2014

---
+++ DesignerRHN/DesignerRHN-ejb/src/java/Entidades/LugaresOcurrencias.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entidades;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Administrador
+ */
+@Entity
+@Table(name = "LUGARESOCURRENCIAS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "LugaresOcurrencias.findAll", query = "SELECT l FROM LugaresOcurrencias l")})
+public class LugaresOcurrencias implements Serializable {
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "SECUENCIA")
+    private BigDecimal secuencia;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CODIGO")
+    private short codigo;
+    @Size(max = 100)
+    @Column(name = "DESCRIPCION")
+    private String descripcion;
+
+    public LugaresOcurrencias() {
+    }
+
+    public LugaresOcurrencias(BigDecimal secuencia) {
+        this.secuencia = secuencia;
+    }
+
+    public LugaresOcurrencias(BigDecimal secuencia, short codigo) {
+        this.secuencia = secuencia;
+        this.codigo = codigo;
+    }
+
+    public BigDecimal getSecuencia() {
+        return secuencia;
+    }
+
+    public void setSecuencia(BigDecimal secuencia) {
+        this.secuencia = secuencia;
+    }
+
+    public short getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(short codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (secuencia != null ? secuencia.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof LugaresOcurrencias)) {
+            return false;
+        }
+        LugaresOcurrencias other = (LugaresOcurrencias) object;
+        if ((this.secuencia == null && other.secuencia != null) || (this.secuencia != null && !this.secuencia.equals(other.secuencia))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entidades.LugaresOcurrencias[ secuencia=" + secuencia + " ]";
+    }
+    
+}
