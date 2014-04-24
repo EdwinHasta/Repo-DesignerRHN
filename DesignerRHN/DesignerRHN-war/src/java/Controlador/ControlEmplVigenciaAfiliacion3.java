c64743fb3e4c0debed0cd568cba70fac2f852b0b
Administrador <Administrador@SOPORTE9>
2014-04-24 10:46:49 -0500

    Felipphe Triviño - 24-04-2014

--- DesignerRHN/DesignerRHN-war/src/java/Controlador/ControlEmplVigenciaAfiliacion3.java
+++ DesignerRHN/DesignerRHN-war/src/java/Controlador/ControlEmplVigenciaAfiliacion3.java
@@ -1269,10 +1269,7 @@ public class ControlEmplVigenciaAfiliacion3 implements Serializable {
             }
             listVigenciasAfiliaciones = null;
             guardado = true;
-<<<<<<< HEAD
-=======
             permitirIndexVA = true;
->>>>>>> origin/master
             RequestContext context = RequestContext.getCurrentInstance();
             context.update("form:ACEPTAR");
             context.update("form:datosVAVigencia");
