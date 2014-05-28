7b5d475876a639c658ece418618f0fec6d8a07a0
user <user@user-PC>
2014-05-28 18:06:52 -0500

    John Pineda

--- DesignerRHN/DesignerRHN-ejb/src/java/Persistencia/PersistenciaFestivos.java
+++ DesignerRHN/DesignerRHN-ejb/src/java/Persistencia/PersistenciaFestivos.java
@@ -1,8 +1,8 @@
 /*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Persistencia;
 
 import Entidades.Festivos;
@@ -15,27 +15,33 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 /**
-*
-* @author user
-*/
+ *
+ * @author user
+ */
 @Stateless
 public class PersistenciaFestivos implements PersistenciaFestivosInterface {
 
     /**
-* Atributo EntityManager. Representa la comunicación con la base de datos.
-*/
+     * Atributo EntityManager. Representa la comunicación con la base de datos.
+     */
     /*@PersistenceContext(unitName = "DesignerRHN-ejbPU")
-private EntityManager em;*/
+     private EntityManager em;*/
     public void crear(EntityManager em, Festivos festivos) {
+        em.getTransaction().begin();
         em.persist(festivos);
+        em.getTransaction().commit();
     }
 
     public void editar(EntityManager em, Festivos festivos) {
+        em.getTransaction().begin();
         em.merge(festivos);
+        em.getTransaction().commit();
     }
 
     public void borrar(EntityManager em, Festivos festivos) {
+        em.getTransaction().begin();
         em.remove(em.merge(festivos));
+        em.getTransaction().commit();
     }
 
     public List<Festivos> consultarFestivosPais(EntityManager em, BigInteger secPais) {
