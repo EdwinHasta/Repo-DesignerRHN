30d5b01b1d6675f59cd12853449828c21868c1aa
Administrador <Administrador@SOPORTE9>
2014-07-31 15:53:25 -0500

    Fabian 31 07 2014

--- DesignerRHN/DesignerRHN-ejb/src/java/Administrar/AdministrarCausasAusentismos.java
+++ DesignerRHN/DesignerRHN-ejb/src/java/Administrar/AdministrarCausasAusentismos.java
@@ -19,11 +19,11 @@ import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 
 @Stateful
-public class AdministrarCausasAusentismos implements AdministrarCausasAusentismosInterface{
+public class AdministrarCausasAusentismos implements AdministrarCausasAusentismosInterface {
 
     @EJB
     PersistenciaClasesAusentismosInterface persistenciaClasesAusentismos;
-    
+
     @EJB
     PersistenciaCausasAusentismosInterface persistenciaCausasausentismos;
 
@@ -36,57 +36,65 @@ public class AdministrarCausasAusentismos implements AdministrarCausasAusentismo
     public void obtenerConexion(String idSesion) {
         em = administrarSesiones.obtenerConexionSesion(idSesion);
     }
+
     @Override
     public List<Causasausentismos> consultarCausasAusentismos() {
         List<Causasausentismos> listaCausasAusentismo;
         listaCausasAusentismo = persistenciaCausasausentismos.buscarCausasAusentismos(em);
         return listaCausasAusentismo;
     }
-    
+
     @Override
     public List<Clasesausentismos> consultarClasesAusentismos() {
         List<Clasesausentismos> listaClasesAusentismos;
         listaClasesAusentismos = persistenciaClasesAusentismos.buscarClasesAusentismos(em);
         return listaClasesAusentismos;
     }
-     
+
     @Override
     public void modificarCausasAusentismos(List<Causasausentismos> listaCausasAusentismo) {
-        Causasausentismos c;
         for (int i = 0; i < listaCausasAusentismo.size(); i++) {
             System.out.println("Modificando...CausasAusentismos");
-            if (listaCausasAusentismo.get(i).getCodigo().equals(null)) {
-                c = listaCausasAusentismo.get(i);
+            System.out.println("posicion : " + i + " secuencia : "+ listaCausasAusentismo.get(i).getSecuencia());
+            if (listaCausasAusentismo.get(i).getCodigo() == (null)) {
+                listaCausasAusentismo.get(i).setCodigo(null);
+                persistenciaCausasausentismos.editar(em, listaCausasAusentismo.get(i));
+            } else if (listaCausasAusentismo.get(i).getClase().getSecuencia() == null) {
+                listaCausasAusentismo.get(i).setClase(null);
             } else {
-                c = listaCausasAusentismo.get(i);
+                persistenciaCausasausentismos.editar(em, listaCausasAusentismo.get(i));
             }
-            persistenciaCausasausentismos.editar(em, c);
         }
     }
-    
+
     @Override
     public void borrarCausasAusentismos(List<Causasausentismos> listaCausasAusentismo) {
         for (int i = 0; i < listaCausasAusentismo.size(); i++) {
             System.out.println("Borrando..CausasAusentismos.");
-            if (listaCausasAusentismo.get(i).getCodigo().equals(null)) {
-
+            if (listaCausasAusentismo.get(i).getCodigo() == (null)) {
+                listaCausasAusentismo.get(i).setCodigo(null);
                 persistenciaCausasausentismos.borrar(em, listaCausasAusentismo.get(i));
+            } else if (listaCausasAusentismo.get(i).getClase().getSecuencia() == null) {
+                listaCausasAusentismo.get(i).setClase(null);
             } else {
                 persistenciaCausasausentismos.borrar(em, listaCausasAusentismo.get(i));
             }
         }
     }
-    
+
     @Override
     public void crearCausasAusentismos(List<Causasausentismos> listaCausasAusentismo) {
         for (int i = 0; i < listaCausasAusentismo.size(); i++) {
             System.out.println("Creando. JornadasLaborales..");
-            if (listaCausasAusentismo.get(i).getCodigo().equals(null)) {
+            if (listaCausasAusentismo.get(i).getCodigo() == (null)) {
+                listaCausasAusentismo.get(i).setCodigo(null);
                 persistenciaCausasausentismos.crear(em, listaCausasAusentismo.get(i));
+            } else if (listaCausasAusentismo.get(i).getClase().getSecuencia() == null) {
+                listaCausasAusentismo.get(i).setClase(null);
             } else {
                 persistenciaCausasausentismos.crear(em, listaCausasAusentismo.get(i));
+            }
         }
     }
-    }  
 
 }
